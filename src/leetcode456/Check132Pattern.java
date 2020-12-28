c07ed31cc129a128ffcdec9a00f8606d21c145bf
chaninkou <michaelkoulove@yahoo.com>
2020-12-27 18:44:07 -0800

    Reedid problem with stack for better understanding.

--- src/leetcode456/Check132Pattern.java
+++ src/leetcode456/Check132Pattern.java
@@ -1,7 +1,44 @@
 package leetcode456;
 
+import java.util.Stack;
+
 public class Check132Pattern {
-	public boolean find132pattern(int[] nums) {
+	public boolean find132pattern(int[] nums){
+        Stack<Integer> stack = new Stack<>();
+        
+        int min = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+        	// Only replaced min when there is smaller number
+            if (num <= min) {
+                min = num;
+            } else {
+                while (!stack.empty()) {
+                	// If 2 > 1
+                    if (stack.peek() >= num){
+                    	break;
+                    }
+                    
+                    stack.pop();
+                    
+                    // If 3 > 2
+                    if (stack.pop() > num){
+                    	return true;
+                    }
+                }
+                
+                // Pretend pushing 3
+                stack.push(num);
+                // Pretend pushing 2
+                stack.push(min);
+            }
+        }
+        
+        // [ 4, 1 ]
+        return false;
+	}
+	
+	public boolean find132pattern2(int[] nums) {
 		int n = nums.length;
 
 		int top = n;
@@ -12,6 +49,7 @@ public class Check132Pattern {
 				return true;
 			}
 
+			// 
 			while (top < n && nums[i] > nums[top]) {
 				third = nums[top];
 				top++;
